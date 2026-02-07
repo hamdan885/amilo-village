b9179ef591988579cb8ea4fe6cb4f02d123f7839
hamdan885 <mohammadhamdanhamdan3@gmail.com>
2026-02-07 17:07:38 +0530

    Create firebase.js

---
+++ js/firebase.js
@@ -0,0 +1,11 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-auth.js";
+
+const firebaseConfig = {
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_PROJECT.firebaseapp.com",
+  projectId: "YOUR_PROJECT_ID",
+};
+
+const app = initializeApp(firebaseConfig);
+window.auth = getAuth(app);
